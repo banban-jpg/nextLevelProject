5456319caec59c89e3f237d7ea54b1d3a9250124
banban-jpg <qkswnsdud@gmail.com>
2026-05-25 19:48:11 +0900

    refactor/ 쿼리문 관리 mybatis로 변경

---
+++ nextLevel/src/main/java/Util/MyBatisUtil.java
@@ -0,0 +1,28 @@
+package util;
+
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MyBatisUtil {
+
+    private static final SqlSessionFactory SQL_SESSION_FACTORY;
+
+    static {
+        try (InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml")) {
+            SQL_SESSION_FACTORY = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (Exception e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    private MyBatisUtil() {
+    }
+
+    public static SqlSession openSession() {
+        return SQL_SESSION_FACTORY.openSession();
+    }
+}
--- nextLevel/src/main/java/concert/dao/ConcertDAO.java
+++ nextLevel/src/main/java/concert/dao/ConcertDAO.java
@@ -1,44 +1,21 @@
 package concert.dao;
 
 import concert.dto.ConcertDTO;
-import util.DbUtil;
+import org.apache.ibatis.session.SqlSession;
+import util.MyBatisUtil;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ConcertDAO {
 
+    private static final String NAMESPACE = "concert.mapper.ConcertMapper.";
+
     public List<ConcertDTO> getAllConcerts() {
-        List<ConcertDTO> concerts = new ArrayList<>();
-        String sql = "SELECT c.CONCERT_ID, c.TITLE, c.DESCRIPTION, c.BOOKING_OPEN_AT, " +
-                     "       c.VENUE_ID, c.ARTIST_NAME, c.GENRE, v.NAME AS VENUE_NAME " +
-                     "FROM CONCERT c " +
-                     "JOIN VENUE v ON c.VENUE_ID = v.VENUE_ID " +
-                     "ORDER BY c.CONCERT_ID DESC";
-
-        try (Connection conn = DbUtil.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql);
-             ResultSet rs = pstmt.executeQuery()) {
-
-            while (rs.next()) {
-                ConcertDTO concert = new ConcertDTO();
-                concert.setConcertId(rs.getInt("CONCERT_ID"));
-                concert.setTitle(rs.getString("TITLE"));
-                concert.setDescription(rs.getString("DESCRIPTION"));
-                concert.setBookingOpenAt(rs.getTimestamp("BOOKING_OPEN_AT"));
-                concert.setVenueId(rs.getInt("VENUE_ID"));
-                concert.setVenueName(rs.getString("VENUE_NAME"));
-                concert.setArtistName(rs.getString("ARTIST_NAME"));
-                concert.setGenre(rs.getString("GENRE"));
-                concerts.add(concert);
-            }
+        try (SqlSession session = MyBatisUtil.openSession()) {
+            return session.selectList(NAMESPACE + "selectAllConcerts");
         } catch (Exception e) {
             e.printStackTrace();
+            return List.of();
         }
-
-        return concerts;
     }
 }
